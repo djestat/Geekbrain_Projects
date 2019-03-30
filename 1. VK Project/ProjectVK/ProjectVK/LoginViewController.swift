106541f3b0340c9e65c1c50fb7c3d4950ea7ae95
Igor <oneseq@gmail.com>
2019-03-30 17:12:27 +0300

    update

--- 1. VK Project/ProjectVK/ProjectVK/LoginViewController.swift
+++ 1. VK Project/ProjectVK/ProjectVK/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  ProjectVK
 //
 //  Created by Igor on 30/03/2019.
@@ -8,13 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
 
 
 }
-
