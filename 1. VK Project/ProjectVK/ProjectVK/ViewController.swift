b82d1d3121379c9ebf49d316dcc85679c05a83a3
Igor <oneseq@gmail.com>
2019-03-30 16:24:52 +0300

    Update

---
+++ 1. VK Project/ProjectVK/ProjectVK/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProjectVK
+//
+//  Created by Igor on 30/03/2019.
+//  Copyright © 2019 Igorlab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
